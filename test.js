04635fc25b57b781111b115e9704a670faf16866
JanakPoojary <janakrajpoojari@gmail.com>
2020-06-20 18:25:41 +0530

    code formated

--- test.js
+++ test.js
@@ -7,9 +7,4 @@ fs.readFile("users.json", function (err, data) {
     // Converting to JSON 
     const users = JSON.parse(data);
     console.log(users.users[0]);
-    // var res = []; 
-
-    //         for(var i in users) 
-    //             res.push(users[i]);
-    //     console.log(res);
 });
